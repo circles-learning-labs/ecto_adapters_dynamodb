50170e2a6c5e77f174bb867d34214d7c0f0d1d2a
Franko Franicevich <franko@franicevich.org>
2017-03-09 19:43:44 -0500

    updated ecto 2.1.4, a little more progress

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule Ecto.Adapters.DynamoDB.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [     
-      {:ecto, "~> 2.0.0"},
+      {:ecto, ">= 2.1.4"},
     ]
   end
 end
