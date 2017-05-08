5be0fce0bdd414cf76bb5b6d566a4385972cbc43
Darren Klein <darren.a.klein@gmail.com>
2017-05-08 14:04:16 -0400

    added mix.lock to .gitignore, added age to schema, added simple insert test

--- test/ecto_adapters_dynamodb_test.exs
+++ test/ecto_adapters_dynamodb_test.exs
@@ -16,6 +16,11 @@ defmodule Ecto.Adapters.DynamoDB.Test do
     assert result.last_name == "Franicevich"
   end
 
+  test "simple insert" do
+    result = TestRepo.insert %Person {id: "person-hello", first_name: "Hello", last_name: "World", age: 34, email: "hello@world.com", password: "password"}
+    assert result == {:ok, %Ecto.Adapters.DynamoDB.TestSchema.Person{age: 34, circles: nil, email: "hello@world.com", first_name: "Hello", id: "person-hello", last_name: "World", password: "password", __meta__: %Ecto.Schema.Metadata{context: nil, source: {nil, "person"}, state: :loaded}}}
+  end
+
   test "get not found" do
     result = TestRepo.get(Person, "person-faketestperson")
     assert result == nil
