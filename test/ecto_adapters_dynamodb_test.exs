7aa8aef37b3cf76d5a20e06f391e6bfc3f943d37
Darren Klein <darren.a.klein@gmail.com>
2017-05-08 15:18:48 -0400

    edited successful case condition on insert_all/batch_write_item

--- test/ecto_adapters_dynamodb_test.exs
+++ test/ecto_adapters_dynamodb_test.exs
@@ -17,8 +17,13 @@ defmodule Ecto.Adapters.DynamoDB.Test do
   end
 
   test "simple insert" do
-    result = TestRepo.insert %Person {id: "person-hello", first_name: "Hello", last_name: "World", age: 34, email: "hello@world.com", password: "password"}
+    result = TestRepo.insert %Person {id: "person-hello", circles: nil, first_name: "Hello", last_name: "World", age: 34, email: "hello@world.com", password: "password"}
     assert result == {:ok, %Ecto.Adapters.DynamoDB.TestSchema.Person{age: 34, circles: nil, email: "hello@world.com", first_name: "Hello", id: "person-hello", last_name: "World", password: "password", __meta__: %Ecto.Schema.Metadata{context: nil, source: {nil, "person"}, state: :loaded}}}
+  end
+
+  test "simple insert_all - multi-record" do
+    result = TestRepo.insert_all(Person, [%{id: "person-buster", circles: nil, first_name: "Buster", last_name: "Diavolo", age: 4, email: "buster@test.com", password: "password"}, %{id: "person-pablo", circles: nil, first_name: "Pablo", last_name: "Martinez", age: 9, email: "pablo@test.com", password: "password"}])
+    assert result == {:ok, []}
   end
 
   test "get not found" do
