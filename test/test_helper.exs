5e26706b5a24a12b4cf720fee34479cac1310ec2
Nick Marino <nick@circl.es>
2017-05-22 18:26:55 -0400

    Fix test code accidentally picking up stray files
    I was getting weird errors when vim left behind .swp files in the
    support directory. This fixes it.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,6 @@
 ExUnit.start()
 
-{:ok, files} = File.ls("./test/support")
+files = File.ls!("./test/support") |> Enum.filter(&(String.ends_with?(&1, [".ex", ".exs"])))
 
 Enum.each files, fn(file) ->
   Code.require_file "support/#{file}", __DIR__
