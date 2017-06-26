356768b15710caf9de059008c4ba30c44420ca19
Gilad Barkan <gilad@giladbarkan.com>
2017-06-26 17:23:36 -0400

    amend function docs, set some functions to private

--- lib/ecto_adapters_dynamodb/query.ex
+++ lib/ecto_adapters_dynamodb/query.ex
@@ -1,7 +1,7 @@
 defmodule Ecto.Adapters.DynamoDB.Query do
   @moduledoc """
   Some query wrapper functions for helping us query dynamo db. Selects indexes to use, etc.
-  Not to be confused with Ecto.Query (Should we rename this module?)
+  Not to be confused with `Ecto.Query`.
 
   """
 
@@ -18,7 +18,7 @@ defmodule Ecto.Adapters.DynamoDB.Query do
 
   # parameters for get_item: 
   # TABLE_NAME::string,
-  # %{LOGICAL_OP::atom => [{ATTRIBUTE::string => {VALUE::string, OPERATOR::atom}}]} | %{ATTRIBUTE::string => {VALUE::string, OPERATOR::atom}}
+  # [{LOGICAL_OP::atom, [{ATTRIBUTE::string, {VALUE::string, OPERATOR::atom}}]} | {ATTRIBUTE::string, {VALUE::string, OPERATOR::atom}}]
   #
 
   # Repo.all(model), provide cached results for tables designated in :cached_tables
@@ -318,7 +318,7 @@ defmodule Ecto.Adapters.DynamoDB.Query do
 
 
   @doc """
-  Given a map containing key values representing a search field, value and operator to search for (e.g., %{"id" => {"franko", :==}}, or %{"circle_id" => {"123", :==}, "person_id" => {"abc", :>}}), will return the dynamo db index description that will help us match this search. return :not_found if no index is found.
+  Given a keyword list containing search field, value and operator to search for (which may also be nested under logical operators, e.g., `[{"id", {"franko", :==}}]`, or `[{:and, [{"circle_id", {"123", :==}}, {"person_id", {"abc", :>}}]}`), will return the dynamo db index description that will help us match this search. return :not_found if no index is found.
 
   Returns a tuple of {"index_name", [ hash_key or hash,range_key]]} or :not_found
   TODO: Does not help with range queries. -> The match_index_hash_part function is
