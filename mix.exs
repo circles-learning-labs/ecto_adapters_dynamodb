71b2635726576ef8a86e43379de0de830343fc94
Franko Franicevich <franko@circl.es>
2017-07-06 14:08:11 -0400

    Updated versions and deps

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Ecto.Adapters.DynamoDB.Mixfile do
 
   def project do
     [app: :ecto_adapters_dynamodb,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
