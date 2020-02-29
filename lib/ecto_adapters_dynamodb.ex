3a23f173f09456f1f51c8c2658506de716fe9994
Gilad Barkan <gilad@giladbarkan.com>
2020-02-29 07:36:49 -0500

    avoid encoding data structures for logs not written

--- lib/ecto_adapters_dynamodb.ex
+++ lib/ecto_adapters_dynamodb.ex
@@ -1236,16 +1236,16 @@ defmodule Ecto.Adapters.DynamoDB do
   Logs message to console and optionally to file. Log levels, colours and file path may be set in configuration (details in README.md).
   """
   def ecto_dynamo_log(level, message, attributes \\ %{}, opts \\ []) do
-    depth = opts[:depth] || 4
-    colours = Application.get_env(:ecto_adapters_dynamodb, :log_colours)
-    d = DateTime.utc_now
-    formatted_message = "#{d.year}-#{d.month}-#{d.day} #{d.hour}:#{d.minute}:#{d.second} UTC [Ecto dynamo #{level}] #{message}"
-    {:ok, log_message} = Jason.encode(%{message: formatted_message, attributes: chisel(attributes, depth)})
-
-    log_path = Application.get_env(:ecto_adapters_dynamodb, :log_path)
     log_levels = Application.get_env(:ecto_adapters_dynamodb, :log_levels) || [:info]
 
     if level in log_levels do
+      log_path = Application.get_env(:ecto_adapters_dynamodb, :log_path)
+      depth = opts[:depth] || 4
+      colours = Application.get_env(:ecto_adapters_dynamodb, :log_colours)
+      d = DateTime.utc_now
+      formatted_message = "#{d.year}-#{d.month}-#{d.day} #{d.hour}:#{d.minute}:#{d.second} UTC [Ecto dynamo #{level}] #{message}"
+      {:ok, log_message} = Jason.encode(%{message: formatted_message, attributes: chisel(attributes, depth)})
+
       if Application.get_env(:ecto_adapters_dynamodb, :log_in_colour) do
         IO.ANSI.format([colours[level] || :normal, log_message], true) |> IO.puts
       else
