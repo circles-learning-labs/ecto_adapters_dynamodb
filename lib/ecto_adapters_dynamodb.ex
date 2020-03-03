circles-learning-labs/ecto_adapters_dynamodb

commit f886c62978de62c9c90ab44d6a00693bd8b2a461

Rename variable, Keyword.get -> Keyword.fetch, version bump to 2.0.0-alpha.2

lib/ecto_adapters_dynamodb.ex
@@ -252,7 +252,7 @@ defmodule Ecto.Adapters.DynamoDB do
 
     # Ecto migration does not know to specify 'scan: true' to retrieve the persisted migration versions
     # from line 34, file "deps/ecto/lib/ecto/migration/schema_migration.ex"
-    migration_source = Keyword.get(repo.config, :migration_source, "schema_migrations")
+    migration_source = Keyword.fetch!(repo.config, :migration_source)
     updated_opts = if table == migration_source do
       ecto_dynamo_log(:debug, "#{inspect __MODULE__}.execute: table name corresponds with migration source: #{inspect migration_source}. Setting options for recursive scan.", %{})
 
@@ -285,8 +285,8 @@ defmodule Ecto.Adapters.DynamoDB do
         else
           case query_meta do
             %{select: %{from: {_, {_, _, _, types}}}} ->
-              fields = types_to_source_fields(model, types)
-              handle_type_decode(table, result, fields)
+              types = types_to_source_fields(model, types)
+              handle_type_decode(table, result, types)
             _ ->
               if table == migration_source do
                 decoded = Enum.map(result["Items"], &(decode_item(&1)))

mix.exs
@@ -3,7 +3,7 @@ defmodule Ecto.Adapters.DynamoDB.Mixfile do
 
   def project do
     [app: :ecto_adapters_dynamodb,
-     version: "2.0.0-alpha.1",
+     version: "2.0.0-alpha.2",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,

commit f1948547b28b778a92577d6209c8f12e916a7d29

Fix bug accidentally introduced in commit f886c62978de62c9c90ab44d6a00693bd8b2a461, remove unnecessary MapSet encoding, efficiency upgrade (avoid encoding unused data structs), version bump to 2.0.0-alpha.3.

lib/ecto_adapters_dynamodb.ex
@@ -252,7 +252,7 @@ defmodule Ecto.Adapters.DynamoDB do
 
     # Ecto migration does not know to specify 'scan: true' to retrieve the persisted migration versions
     # from line 34, file "deps/ecto/lib/ecto/migration/schema_migration.ex"
-    migration_source = Keyword.fetch!(repo.config, :migration_source)
+    migration_source = Keyword.get(repo.config, :migration_source, "schema_migrations")
     updated_opts = if table == migration_source do
       ecto_dynamo_log(:debug, "#{inspect __MODULE__}.execute: table name corresponds with migration source: #{inspect migration_source}. Setting options for recursive scan.", %{})
 

mix.exs
@@ -3,7 +3,7 @@ defmodule Ecto.Adapters.DynamoDB.Mixfile do
 
   def project do
     [app: :ecto_adapters_dynamodb,
-     version: "2.0.0-alpha.2",
+     version: "2.0.0-alpha.3",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,