65b05c5d6f1f35f8fcabb4184d2f44584a1a0ef1
Darren Klein <darren.a.klein@gmail.com>
2020-03-15 19:15:08 -0400

    Add tests for ExAws.Dynamo.Decoder, add integration test for Jason.

--- test/integration/ex_aws_dynamo_test.exs
+++ test/integration/ex_aws_dynamo_test.exs
@@ -7,29 +7,41 @@ defmodule Ecto.Adapters.DynamoDB.Integration.ExAws.Dynamo.Test do
 
   @ex_aws_dynamo_test_table_name "ex_aws_dynamo_test_table"
 
-  describe "Integration tests for ExAws.Dynamo" do
-    test "create_table" do
-      Dynamo.create_table(@ex_aws_dynamo_test_table_name, [email: :hash, age: :range], [email: :string, age: :number], 1, 1) |> ExAws.request!
-      {:ok, table_info} = ExAws.Dynamo.describe_table(@ex_aws_dynamo_test_table_name) |> ExAws.request()
+  test "create_table" do
+    Dynamo.create_table(@ex_aws_dynamo_test_table_name, [email: :hash, age: :range], [email: :string, age: :number], 1, 1) |> ExAws.request!
+    {:ok, table_info} = ExAws.Dynamo.describe_table(@ex_aws_dynamo_test_table_name) |> ExAws.request()
 
-      assert table_info["Table"]["TableName"] == @ex_aws_dynamo_test_table_name
-      assert table_info["Table"]["BillingModeSummary"]["BillingMode"] == "PROVISIONED"
-    end
-
-    test "update_table" do
-      Dynamo.update_table(@ex_aws_dynamo_test_table_name, [billing_mode: :pay_per_request]) |> ExAws.request!
-      {:ok, table_info} = ExAws.Dynamo.describe_table(@ex_aws_dynamo_test_table_name) |> ExAws.request()
-
-      assert table_info["Table"]["BillingModeSummary"]["BillingMode"] == "PAY_PER_REQUEST"
-    end
-
-    test "delete_table" do
-      result = Dynamo.delete_table(@ex_aws_dynamo_test_table_name) |> ExAws.request!
-
-      assert result["TableDescription"]["TableName"] == @ex_aws_dynamo_test_table_name
-    end
-
-    # TODO - add tests for Dynamo.Decoder.decode()
+    assert table_info["Table"]["TableName"] == @ex_aws_dynamo_test_table_name
+    assert table_info["Table"]["BillingModeSummary"]["BillingMode"] == "PROVISIONED"
   end
-  
+
+  test "update_table" do
+    Dynamo.update_table(@ex_aws_dynamo_test_table_name, [billing_mode: :pay_per_request]) |> ExAws.request!
+    {:ok, table_info} = ExAws.Dynamo.describe_table(@ex_aws_dynamo_test_table_name) |> ExAws.request()
+
+    assert table_info["Table"]["BillingModeSummary"]["BillingMode"] == "PAY_PER_REQUEST"
+  end
+
+  test "delete_table" do
+    result = Dynamo.delete_table(@ex_aws_dynamo_test_table_name) |> ExAws.request!
+
+    assert result["TableDescription"]["TableName"] == @ex_aws_dynamo_test_table_name
+  end
+
+  test "Decoder.decode()" do
+    assert Dynamo.Decoder.decode(%{"BOOL" => true}) == true
+    assert Dynamo.Decoder.decode(%{"BOOL" => false}) == false
+    assert Dynamo.Decoder.decode(%{"BOOL" => "true"}) == true
+    assert Dynamo.Decoder.decode(%{"BOOL" => "false"}) == false
+    assert Dynamo.Decoder.decode(%{"NULL" => true}) == nil
+    assert Dynamo.Decoder.decode(%{"NULL" => "true"}) == nil
+    assert Dynamo.Decoder.decode(%{"B" => "Zm9vYmFy"}) == "foobar"
+    assert Dynamo.Decoder.decode(%{"S" => "foo"}) == "foo"
+    assert Dynamo.Decoder.decode(%{"M" => %{"M" => %{foo: %{"S" => "bar"}}}}) == %{foo: "bar"}
+    assert Dynamo.Decoder.decode(%{"BS" => ["U3Vubnk=", "UmFpbnk=", "U25vd3k="]}) == MapSet.new(["U3Vubnk=", "UmFpbnk=", "U25vd3k="])
+    assert Dynamo.Decoder.decode(%{"SS" => ["foo", "bar", "baz"]}) == MapSet.new(["foo", "bar", "baz"])
+    assert Dynamo.Decoder.decode(%{"NS" => [1, 2, 3]}) == MapSet.new([1, 2, 3])
+    assert Dynamo.Decoder.decode(%{"NS" => ["1", "2", "3"]}) == MapSet.new([1, 2, 3])
+    assert Dynamo.Decoder.decode(%{"L" => [%{"S" => "asdf"}, %{"N" => "1"}]}) == ["asdf", 1]
+  end
 end
