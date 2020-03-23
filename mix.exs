89d5eb4a266810655e212ba90c560728a17a523e
Darren Klein <darren.a.klein@gmail.com>
2020-03-23 17:24:44 -0400

    Version bump -> 2.0.0-beta.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Ecto.Adapters.DynamoDB.Mixfile do
 
   def project do
     [app: :ecto_adapters_dynamodb,
-     version: "2.0.0-alpha.4",
+     version: "2.0.0-beta.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
